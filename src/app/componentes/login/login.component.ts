f778b2e79c7d3e6280648aa861ca558d2305d2ed
0022Ale <alejandra.sagasta@gmail.com>
2022-10-31 21:58:19 -0300

    Finalizando

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
-
+  constructor(private formBuilder: FormBuilder) {
+    this.form = formBuilder.group({
+      password:['123456',[]],
+      mail:['pruebita@gmail.com', []]
+   })
+  }
   ngOnInit(): void {
   }
-
 }
